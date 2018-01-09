745395e8d0f82d549204e127ec6876dfbf955f6e
Aleksander <aleks4@outlook.com>
2018-01-09 23:04:27 +0100

    Dalsze komentarze

--- terminarz/main.cpp
+++ terminarz/main.cpp
@@ -1,6 +1,11 @@
 #include "mainwindow.h"
 #include <QApplication>
-
+/*!
+ * \brief Funkcja main
+ *
+ * Rdzeń całego programu, jednakże cała jej rola sprowadza się do utworzenia okna i doprowadzenia do jego działania.
+ *
+ */
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
--- terminarz/startuplistevents.cpp
+++ terminarz/startuplistevents.cpp
@@ -1,7 +1,9 @@
 #include "startuplistevents.h"
 #include "ui_startuplistevents.h"
 #include <QMessageBox>
-
+/*!
+ * \brief Typ numerujący nagłówki kolumn
+ */
 enum header_names_startup {DATE,GODZ,DESC,STAN};
 
 startupListEvents::startupListEvents(QString session_username, QWidget *parent) :
@@ -9,19 +11,19 @@ startupListEvents::startupListEvents(QString session_username, QWidget *parent) 
     ui(new Ui::startupListEvents)
 {
     ui->setupUi(this);
-    setWindowTitle("Nadchodzące wydarzenia");
-    ui->tableWidget->setColumnCount(4);
-    ui->tableWidget->setRowCount(0);
-    ui->tableWidget->verticalHeader()->setVisible(false);
-    QStringList headers;
-    headers<<"Data"<<"Godzina"<<"Opis zadania"<<"Stan";
-    ui->tableWidget->setHorizontalHeaderLabels(headers);
-    ui->tableWidget->setColumnWidth(GODZ, 60);
-    ui->tableWidget->setColumnWidth(DESC,300);
-    ui->tableWidget->setColumnWidth(DATE, 200);
+    setWindowTitle("Nadchodzące wydarzenia"); //!< Ustawianie nazwy okna
+    ui->tableWidget->setColumnCount(4); //!< Ustawianie ilości kolumn
+    ui->tableWidget->setRowCount(0); //!< Ustawienie początkowej ilości wierszy
+    ui->tableWidget->verticalHeader()->setVisible(false); //!< Usuwanie nagłówka pionowego
+    QStringList headers; //!< Przechowuje nagłówki
+    headers<<"Data"<<"Godzina"<<"Opis zadania"<<"Stan"; //!< Wczytanie nazw nagłówków.
+    ui->tableWidget->setHorizontalHeaderLabels(headers); //!< Wczytanie listy z nagłówkami kolumn tabeli
+    ui->tableWidget->setColumnWidth(GODZ, 60); //!<Rozmiar kolumny godziny
+    ui->tableWidget->setColumnWidth(DESC,300); //!<Rozmiar kolumny na opis wydarzenia
+    ui->tableWidget->setColumnWidth(DATE, 200); //!< Rozmiar kolumny daty
     ui->tableWidget->setColumnWidth(STAN, 110);
-    readFile(archive);
-    session_data=session_username;
+    readFile(archive); //!< Odczyt wydarzeń z pliku
+    session_data=session_username; //!< Ustalenie nazwy bieżącego użytkownika
     fetchTable(QDate::currentDate());
 }
 
@@ -36,35 +38,31 @@ QDataStream &operator>>(QDataStream &in, event_log_struct &buffer)
 }
 bool startupListEvents::readFile(QFile &archive)
 {
-    archive.setFileName("archive.txt");
+    archive.setFileName("archive.txt"); //!<Ustawienie nazwy pliku
     if(archive.exists() == false)
     {
-        /*QMessageBox warn;
-        warn.setText("Odnotowano pierwsze uruchomienie.");
-        warn.setInformativeText("Zostanie utworzony plik do przechowania danych w chwili dodania pierwszego wpisu.");
-        warn.exec();*/
         return 1;
     }
     else{
-        if(archive.open(QIODevice::Text | QIODevice::ReadOnly)==false)
+        if(archive.open(QIODevice::Text | QIODevice::ReadOnly)==false) //!< Otwieranie pliku i sprawdzenie statusu powodzenia operacji
         {
-            QMessageBox error;
+            QMessageBox error; //!<Generowanie komunikatu o błędzie w przypadku niepowodzenia
             error.setText("Fatal error!");
             error.setInformativeText("Niepowodzenie w otwarciu pliku do odczytu.");
-            QApplication::exit(-1);
+            QApplication::exit(-1); //!< Awaryjne zamknięcie aplikacji
         }
         else
         {
-            event_log_struct buffer;
+            event_log_struct buffer; //!< Zmienna tymczasowa na przechowywanie wpisów
             QDataStream plik(&archive);
-            while(!plik.atEnd())
+            while(!plik.atEnd()) //!< Odczytywanie kolejnych danych z pliku
             {
                 plik>>buffer;
                 storage.push_back(buffer);
 
             }
             qDebug()<<"[startuplistevents] Wpisano wydarzenia do okna powitalnego"<<endl;
-            archive.close();
+            archive.close(); //!< Zamknięcie pliku
             return 1;
         }
     }
@@ -72,13 +70,17 @@ bool startupListEvents::readFile(QFile &archive)
 }
 void startupListEvents::fetchTable(const QDate &date)
 {
-    int rowsCount=0;
+    int rowsCount=0; //!<Początkowa ilość wierszy
     for(int i=0; i<storage.size(); i++)
     {
         qDebug()<<"[startuplistevents] fetchTable: do porównania: "<<endl;
         qDebug()<<"-> storage[i].date = "<<storage[i].date.toString()<<" z "<< date.toString()<<endl;
         qDebug()<<"-> storage[i].time = "<<storage[i].time.toString()<<" z "<<QTime::currentTime()<<endl;
         qDebug()<<"-> Data o dzień późniejsza: "<<storage[i].date.addDays(1)<<" i "<<QDate::currentDate().addDays(1)<<endl;
+        /*!
+         * Wczytywanie wydarzeń na bieżący dzień
+         *
+         */
         if((storage[i].date == date) && (storage[i].time >= QTime::currentTime()) && (storage[i].username==session_data))
         {
             ui->tableWidget->setRowCount(++rowsCount);
@@ -90,7 +92,7 @@ void startupListEvents::fetchTable(const QDate &date)
 
         }
     }
-
+    //! Wczytywanie wydarzeń na dzień późniejszy
     for(int i=0; i<storage.size(); i++)
     {
         if(storage[i].date==date.addDays(1))
